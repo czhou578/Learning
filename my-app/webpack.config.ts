318f53e62fe0933ec50391a944204eaee10d22c3
czhou578 <colizu2020@gmail.com>
2021-12-26 14:14:50 -0800

    changed webpack.config

--- my-app/webpack.config.ts
+++ my-app/webpack.config.ts
@@ -1,19 +1,46 @@
 const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   mode: 'development',
   devServer: {
     compress: true,
-    port: 300,
-    historyApiFallback: true,
+    port: 3000,
+    historyApiFallback: true
   },
   entry: __dirname + '/src/index.js',
   module: {
     rules: [
-      { test: /\.tsx?$/, use: ['ts-loader'] },
-      { test: /\.css$/, use: ['style-loader', {loader: 'css-loader', options: {modules: true}}]}
-    ]
-  }
-}
+      {
+        test: /\.tsx?$/,
+        use: ['ts-loader']
+      },
+      {
+        test: /\.css?$/,
+        use: [
+          'style-loader',
+          {
+            loader: 'css-loader',
+            options: {
+              modules: true
+            }
+          }
+        ]
 
+      }
+    ]
+  },
+  devtool: 'inline-source-map',
+  resolve: {
+    extensions: ['.js', '.tsx', '.css', '.ts']
+  },
+  target: 'web',
+  output: {
+    filename: "[name].[contenthash].js",
+    publicPath: "/"
+  },
+  plugins: [
+    new HtmlWebpackPlugin({template: './public/index.html'}),
+  ]
+}
 
