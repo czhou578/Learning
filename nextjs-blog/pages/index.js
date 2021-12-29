4f552a02dd5341bc8e16d5aa37f6f0e052e8a743
czhou578 <colizu2020@gmail.com>
2021-12-28 21:58:57 -0800

    parsed md files

--- nextjs-blog/pages/index.js
+++ nextjs-blog/pages/index.js
@@ -1,21 +1,50 @@
 import Head from 'next/head'
 import Layout, {siteTitle} from '../components/layout'
 import utilStyles from '../styles/utils.module.css'
+import { getSortedPostsData } from '../lib/posts'
 
-export default function Home() {
+export async function getStaticProps() {
+  const allPostsData = getSortedPostsData()
+  return {
+    props: {
+      allPostsData
+    }
+  }
+}
+export default function Home({ allPostsData }) {
   return (
+
     <Layout home>
-      <Head>
-        <title>{siteTitle}</title>
-      </Head>
-      <section className={utilStyles.headingMd}>
-        <p>I'm a student and web developer</p>
-        <p>
-        (Go ahead and visit my website below!{' '}
-        <a href='https://www.czhou578.com'>My website</a>)
-        </p>
-      </section>
-    </Layout>
+    {/* Keep the existing code here */}
+
+    {/* Add this <section> tag below the existing <section> tag */}
+    <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+      <h2 className={utilStyles.headingLg}>Blog</h2>
+      <ul className={utilStyles.list}>
+        {allPostsData.map(({ id, date, title }) => (
+          <li className={utilStyles.listItem} key={id}>
+            {title}
+            <br />
+            {id}
+            <br />
+            {date}
+          </li>
+        ))}
+      </ul>
+    </section>
+  </Layout>
+    // <Layout home>
+    //   <Head>
+    //     <title>{siteTitle}</title>
+    //   </Head>
+    //   <section className={utilStyles.headingMd}>
+    //     <p>I'm a student and web developer</p>
+    //     <p>
+    //     (Go ahead and visit my website below!{' '}
+    //     <a href='https://www.czhou578.com'>My website</a>)
+    //     </p>
+    //   </section>
+    // </Layout>
 
     // <div className="container">
     //   <Head>
