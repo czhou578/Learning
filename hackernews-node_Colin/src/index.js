69993b991713585bdb2ef675f0c599196a519a7c
czhou578 <colizu2020@gmail.com>
2022-06-21 16:12:38 -0700

    added graphql folder

---
+++ hackernews-node_Colin/src/index.js
@@ -0,0 +1,45 @@
+const { ApolloServer } = require("apollo-server"); //graphql express server
+
+const typeDefs = `
+  type Query {
+    info: String!
+    feed: [Link!]!
+  }
+
+  type Link {
+    id: ID!
+    description: String!
+    url: String!
+  }
+`;
+
+let links = [
+  {
+    id: "link-0",
+    url: "www.hotographql.com",
+    description: "Fullstack tutorial for GraphQL",
+  },
+];
+
+const resolvers = {
+  Query: {
+    info: () => `This is the API of a Hackernews Clone`,
+    // info: () => null,
+    feed: () => links,
+  },
+
+  Link: {
+    id: (parent) => parent.id,
+    description: (parent) => parent.description,
+    url: (parent) => parent.url,
+  },
+};
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+});
+
+server.listen().then(({ url }) => {
+  console.log(`Server is running on ${url}`);
+});
