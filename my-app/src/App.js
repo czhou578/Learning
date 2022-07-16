5671e2afc2663dbfdb56ec9b32090963d3d2c8c5
czhou578 <colizu2020@gmail.com>
2022-07-15 17:33:50 -0700

    internship work

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect } from "react";
+import "./App.css";
 
 function App() {
+  useEffect(() => {
+    const script = document.createElement("script");
+    script.src = "my-app/src/print.js";
+    script.async = true;
+    document.body.appendChild(script);
+    return () => {
+      document.body.removeChild(script);
+    };
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* <script src="script.js"></script> */}
+      <div>hello</div>
     </div>
   );
 }
---
+++ my-app/src/script.py
@@ -0,0 +1,4 @@
+import requests
+
+response = requests.get('https://randomuser.me/api')
+print(response.json())
