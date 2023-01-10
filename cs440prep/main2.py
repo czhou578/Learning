0ea091ea322d48a7e73da987e4b91607dfcbce86
czhou578 <colizu2020@gmail.com>
2023-01-10 09:58:53 -0800

    continuing python learning

--- cs440prep/main2.py
+++ cs440prep/main2.py
@@ -27,3 +27,33 @@ print(f'{year}')
 print("hello, this is {0}".format("Colin"))
 
 json.dumps([1, 'simple', 'list'])
+
+# while True:
+#     try:
+#         x = int(input("enter a number"))
+#         break
+#     except ValueError:
+#         print("Oop! Try again")
+
+# while True:
+#     try:
+#         y = "this is true"
+#         break
+#     except ValueError:
+#         print("yikes")
+
+# raise Exception("hello")
+# raise NameError("hasdf")
+
+
+def divide(x, y):
+    try:
+        result = x / y
+    except ZeroDivisionError:
+        print("division by zero")
+    else:
+        print("result is ", result)
+    finally:
+        print("executing finally clause")
+
+# libraries: mathematics, (data compression), zlib, gzip, tarfile, logging
